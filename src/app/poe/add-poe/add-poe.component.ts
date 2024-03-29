19da63b6da52fc85be50b1ae396c412a53e03a10
Loic <loic.grd@gmail.com>
2024-03-29 14:42:06 +0100

    formulaire ajout poe avec conditions

--- src/app/poe/add-poe/add-poe.component.ts
+++ src/app/poe/add-poe/add-poe.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { PoeService } from '../services/poe.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Poe } from '../types/poe-types';
 
 @Component({
   selector: 'app-add-poe',
@@ -8,9 +10,44 @@ import { PoeService } from '../services/poe.service';
   styleUrl: './add-poe.component.scss'
 })
 export class AddPoeComponent {
+  public poeForm: FormGroup = new FormGroup({}) //FormGRoup permet de manipuler le formulaire dans son ensemble
+
+
 
 
   constructor(
-    private _router: Router
-  ){}
+    private _router: Router,
+    private _poeService: PoeService,
+    private _formBuilder: FormBuilder
+
+    ){}
+
+ngOnInit(): void { //méthode appliqué a tous les composants et est executer immediatement après le constructeur de la classe (logique de contruction)
+  this.poeForm = this._formBuilder.group({ //{} = objet
+    poename: [ //tableau qui contient la aleur par défaut et un autre tableau controlant les validateurs
+      '', //Default value (champ vide)
+      [
+        Validators.required, //Ajout de validateurs
+        Validators.minLength(3), //Ajout de patterns et est non visible sur le code source ici
+      ]
+    ],
+    poeduration: [
+      '',
+      [
+        Validators.required,
+        Validators.min(1)
+      ]
+    ]
+  }) 
+}    
+
+
+onSubmit(): void{ //onSubmit : Lorsque la soumission du formulaire arrivera
+  this._poeService.add(this.poeForm.value) //Ajout du service d'ajout de nom dans le tableau
+    .subscribe((intern: Poe)=> {
+      this._router.navigate(['/home'])
+    })
+  this._router.navigate(['/home']) //pour revenir vers home quand on ajoute quelqu'un a la liste
+  }
+
 }
--- src/app/poe/services/poe.service.ts
+++ src/app/poe/services/poe.service.ts
@@ -2,12 +2,26 @@ import { Injectable } from '@angular/core';
 import { IService } from '../../core/interfaces/i-service';
 import { Poe } from '../types/poe-types';
 import { HttpClient } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PoeService  {
-  private _poes: Array<Poe> = []
+  private _poes: Array<Poe> = [
+    {
+    poename : 'cybersecurite',
+    poeduration: 400
+    }
+
+  ]
+
+  add(item: Poe): Observable<Poe> {
+    this._poes.push(item) //permet d'ajouter le nom au tableau d'interns
+    return of(item)
+    
+  }
+
 
   constructor() { }
     private _httpClient: HttpClient | undefined
